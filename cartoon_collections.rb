1e0b7a845530db3fd4cbc0bc0ef61c3dfa5283a1
Duke Ko <dukhyunko93@gmail.com>
2020-06-19 14:53:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,5 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_valid_calls(planeteer_calls)
-  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find {|e| valid_calls.include?(e)}
 end
